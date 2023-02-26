d45578e1b7d15d086456b3bf03806e295725c7e9
Alexpotrebitel <118938387+Alexpotrebitel@users.noreply.github.com>
2023-02-27 02:24:00 +0500

    ew

--- Program.cs
+++ Program.cs
@@ -1,40 +1,68 @@
-﻿
-   void Main(string[] args)
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
     {
-        // Вводим исходный массив строк с клавиатуры
-        Console.Write("Введите элементы массива через пробел: ");
-        string input = Console.ReadLine();
-        string[] a = input.Split(' ');
+        string[] array = ReadArray(); 
+        PrintArray(array); 
+        string[] newArray = FilterArray(array); 
+        PrintArray(newArray); 
 
-        // Создаем новый массив, содержащий только строки длиной <= 3
-        string[] b = new string[a.Length];
-        int count = 0;
+        Console.ReadKey(); 
+    }
 
-        for (int i = 0; i < a.Length; i++)
+  
+    static string[] ReadArray()
+    {
+        Console.Write("Введите размер массива: ");
+        int n = int.Parse(Console.ReadLine()); 
+
+        string[] array = new string[n]; 
+        for (int i = 0; i < n; i++) 
         {
-            if (a[i].Length <= 3)
+            Console.Write($"Введите строку {i + 1}: ");
+            array[i] = Console.ReadLine();
+        }
+
+        return array; 
+    }
+    
+    
+    static string[] FilterArray(string[] array)
+    {
+        int count = 0; 
+                foreach (string s in array) 
+        {
+            if (s.Length <= 3) 
             {
-                b[count] = a[i];
                 count++;
             }
         }
 
-        Array.Resize(ref b, count);
+        string[] newArray = new string[count]; 
 
-        void Show
-        Console.WriteLine("Исходный массив:");
-        PrintArray(a);
+        int j = 0; 
+        foreach (string s in array) 
+        {
+            if (s.Length <= 3) 
+            {
+                newArray[j] = s;
+                j++; 
+            }
+        }
 
-        Console.WriteLine("Новый массив:");
-        PrintArray(b);
+        return newArray; 
     }
 
+    
     static void PrintArray(string[] array)
     {
-        Console.Write("[");
-        for (int i = 0; i < array.Length; i++)
+        Console.WriteLine("\nМассив строк:");
+        foreach (string s in array) 
         {
-            Console.Write(array[i] + " ");
+            Console.WriteLine(s);
         }
-        Console.WriteLine("]");
     }
+
+}
